db2f6901ab0d1b0321cfb33d897da6ff2231be26
hywmarsh <hywmarsh@gmail.com>
2016-04-12 21:56:00 +0100

    English version

---
+++ prjRepeatUntil/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer = 0
+        Dim Number As Integer = 0
+
+        '
+        Randomize()
+        Number = Rnd() * 100 \ 10
+        MsgBox(Number)
+        Do
+            i = InputBox("Guess a number")
+            If i <> Number Then
+                MsgBox("Incorrect")
+            End If
+
+        Loop Until i = Number
+
+    End Sub
+End Class
